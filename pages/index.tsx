73b4941a0760638998f9bedbd9543c9432d874c3
ChanelJ1506 <99685983+ChanelJ1506@users.noreply.github.com>
2024-08-14 16:45:18 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Next.js App</title>
+      </Head>
+
+      <h1>Welcome to my Next.js App</h1>
+    </div>
+  )
+}
